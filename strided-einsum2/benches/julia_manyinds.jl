0731aa60eaf9769f74e8a3226e26861c929b854c
SatoshiTerasaki <terasakisatoshi.math@gmail.com>
2026-02-06 13:07:59 +0900

    Try to implement benchmark for strided-einsum2

---
+++ strided-einsum2/benches/julia_manyinds.jl
@@ -0,0 +1,17 @@
+using OMEinsum
+using BenchmarkTools
+using Statistics
+using Random
+
+@info "Julia Threads: $(Threads.nthreads())"
+
+function bench_manyinds(T::DataType)
+    code = ein"abcdefghijklmnop,flnqrcipstujvgamdwxyz->bcdeghkmnopqrstuvwxyz"
+    arr1 = rand(T, map(i->2, OMEinsum.getixs(code)[1])...)
+    arr2 = rand(T, map(i->2, OMEinsum.getixs(code)[2])...)
+    t = @benchmark $code($arr1,$arr2)
+    println("manyinds (OMEinsum): ", mean(t.times) / 1e6, " ms")
+end
+
+bench_manyinds(Float64)
+bench_manyinds(ComplexF64)
